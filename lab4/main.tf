7b7f74cd82a40731b149db112c64877537b7a303
Neelima <Neelima.waghchoude@gmail.com>
2024-12-10 15:44:42 +0000

    adding lab4

--- lab4/main.tf
+++ lab4/main.tf
@@ -11,6 +11,7 @@ terraform {
 }
 
 provider "azurerm" {
+  subscription_id = "d59e013b-b73f-45f3-ba58-1a3a7575a861"
   features {}
 }
 
@@ -21,29 +22,29 @@ resource "azurerm_resource_group" "resgrp" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "myVNet"
-  resource_group_name = "RG4"
-  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.resgrp.name
+  location            = azurerm_resource_group.resgrp.location
   address_space       = ["10.21.0.0/16"]
 }
 
 resource "azurerm_subnet" "frontend" {
   name                 = "myAGSubnet"
-  resource_group_name  = "RG4"
-  virtual_network_name = "myVNet"
+  resource_group_name  = azurerm_resource_group.resgrp.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.21.0.0/24"]
 }
 
 resource "azurerm_subnet" "backend" {
   name                 = "myBackendSubnet"
-  resource_group_name  = "RG4"
-  virtual_network_name = "myVNet"
+  resource_group_name  = azurerm_resource_group.resgrp.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.21.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pubip" {
   name                = "myAGPublicIPAddress"
-  resource_group_name = "RG4"
-  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.resgrp.name
+  location            = azurerm_resource_group.resgrp.location
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -51,8 +52,8 @@ resource "azurerm_public_ip" "pubip" {
 
 resource "azurerm_application_gateway" "appgtw" {
   name                = "myAppGateway"
-  resource_group_name = "RG4"
-  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.resgrp.name
+  location            = azurerm_resource_group.resgrp.location
 
   sku {
     name     = "Standard_v2"
@@ -66,21 +67,21 @@ resource "azurerm_application_gateway" "appgtw" {
   }
 
   frontend_port {
-    name = "qalabFrontendPort"
+    name = var.frontend_port_name
     port = 80
   }
 
   frontend_ip_configuration {
-    name                 = "qalabAGIPConfig"
+    name                 = var.frontend_ip_configuration_name
     public_ip_address_id = azurerm_public_ip.pubip.id
   }
 
   backend_address_pool {
-    name = "qalabBackendPool"
+    name = var.backend_address_pool_name
   }
 
   backend_http_settings {
-    name                  = "qalabHTTPsetting"
+    name                  = var.http_setting_name
     cookie_based_affinity = "Disabled"
     port                  = 80
     protocol              = "Http"
@@ -88,26 +89,42 @@ resource "azurerm_application_gateway" "appgtw" {
   }
 
   http_listener {
-    name                           = "qalabListener"
-    frontend_ip_configuration_name = "qalabAGIPConfig"
-    frontend_port_name             = "qalabFrontendPort"
+    name                           = var.listener_name
+    frontend_ip_configuration_name = var.frontend_ip_configuration_name
+    frontend_port_name             = var.frontend_port_name
     protocol                       = "Http"
   }
 
   request_routing_rule {
-    name                       = "qalabRoutingRule"
+    name                       = var.request_routing_rule_name
     rule_type                  = "Basic"
-    http_listener_name         = "qalabListener"
-    backend_address_pool_name  = "qalabBackendPool"
-    backend_http_settings_name = "qalabHTTPsetting"
+    http_listener_name         = var.listener_name
+    backend_address_pool_name  = var.backend_address_pool_name
+    backend_http_settings_name = var.http_setting_name
     priority = 10
   }
 }
 
-resource "azurerm_network_interface" "nic1" {
+resource "azurerm_network_interface" "nic" {
+  for_each = tomap({
+    "VM1"= "nic-vm1"
+    "VM2"="nic-vm2"
+  })
+  name = each.value
+  location = azurerm_resource_group.resgrp.location
+  resource_group_name = azurerm_resource_group.resgrp.location
+
+  ip_configuration {
+    name = "${each.key}-ipconfig"
+    subnet_id = azurerm_subnet.backend.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+/*resource "azurerm_network_interface" "nic1" {
   name                = "nic-1"
-  location            = "westeurope"
-  resource_group_name = "RG4"
+  location            = azurerm_resource_group.resgrp.location
+  resource_group_name = azurerm_resource_group.resgrp.name
 
   ip_configuration {
     name                          = "nic-ipconfig-1"
@@ -118,18 +135,24 @@ resource "azurerm_network_interface" "nic1" {
 
 resource "azurerm_network_interface" "nic2" {
   name                = "nic-2"
-  location            = "westeurope"
-  resource_group_name = "RG4"
+  location            = azurerm_resource_group.resgrp.location
+  resource_group_name = azurerm_resource_group.resgrp.name
 
   ip_configuration {
     name                          = "nic-ipconfig-2"
     subnet_id                     = azurerm_subnet.backend.id
     private_ip_address_allocation = "Dynamic"
   }
+}*/
+
+resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "nic-assoc" {
+  for_each = azurerm_network_interface.nic
+  network_interface_id = each.value.id
+  ip_configuration_name = "${each.key}-ipconfig"
+  backend_address_pool_id = one(azurerm_application_gateway.appgtw.backend_address_pool).id
 }
 
-
-resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "nic-assc1" {
+/*resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "nic-assc1" {
   network_interface_id    = azurerm_network_interface.nic1.id
   ip_configuration_name   = "nic-ipconfig-1"
   backend_address_pool_id = tolist(azurerm_application_gateway.appgtw.backend_address_pool).0.id
@@ -139,7 +162,7 @@ resource "azurerm_network_interface_application_gateway_backend_address_pool_ass
   network_interface_id    = azurerm_network_interface.nic2.id
   ip_configuration_name   = "nic-ipconfig-2"
   backend_address_pool_id = tolist(azurerm_application_gateway.appgtw.backend_address_pool).0.id
-}
+}*/
 
 resource "random_password" "password" {
   length = 16
@@ -149,10 +172,36 @@ resource "random_password" "password" {
   numeric = true
 }
 
-resource "azurerm_windows_virtual_machine" "vm1" {
+resource "azurerm_windows_virtual_machine" "VM" {
+  for_each = toset(["VM1","VM2"])
+  name = each.value
+  resource_group_name = azurerm_resource_group.resgrp.name
+  location = azurerm_resource_group.resgrp.location
+  size="Standard_DS2_v2"
+  admin_username = "azureadmin"
+  admin_password = random_password.password.result
+
+  network_interface_ids = [
+    azurerm_network_interface.nic[each.key].id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+/*resource "azurerm_windows_virtual_machine" "vm1" {
   name                = "VM1"
-  resource_group_name = "RG4"
-  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.resgrp.name
+  location            = azurerm_resource_group.resgrp.location
   size                = "Standard_DS2_v2"
   admin_username      = "azureadmin"
   admin_password      = random_password.password.result
@@ -177,8 +226,8 @@ resource "azurerm_windows_virtual_machine" "vm1" {
 
 resource "azurerm_windows_virtual_machine" "vm2" {
   name                = "VM2"
-  resource_group_name = "RG4"
-  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.resgrp.name
+  location            = azurerm_resource_group.resgrp.location
   size                = "Standard_DS2_v2"
   admin_username      = "azureadmin"
   admin_password      = random_password.password.result
@@ -199,10 +248,24 @@ resource "azurerm_windows_virtual_machine" "vm2" {
     sku       = "2019-Datacenter"
     version   = "latest"
   }
+}*/
+
+resource "azurerm_virtual_machine_extension" "vm-extensions" {
+  for_each = toset(["VM1","VM2"])
+  name = "${each.value}-ext"
+  virtual_machine_id = azurerm_windows_virtual_machine.VM[each.key].id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"
+    }
+SETTINGS
 }
 
-
-resource "azurerm_virtual_machine_extension" "vm-extensions1" {
+/*resource "azurerm_virtual_machine_extension" "vm-extensions1" {
   name                 = "vm1-ext"
   virtual_machine_id   = azurerm_windows_virtual_machine.vm1.id
   publisher            = "Microsoft.Compute"
@@ -228,7 +291,7 @@ resource "azurerm_virtual_machine_extension" "vm-extensions2" {
         "commandToExecute": "powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"
     }
 SETTINGS
-}
+}*/
 
 output "appgw_public_ip_address" {
   description = "The public IP address of Application Gateway."
